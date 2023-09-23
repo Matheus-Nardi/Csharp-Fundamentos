0d769721590a8bf403faa90170e9f4fdfc9e7f00
Matheus <matheuznardi@gmail.com>
2023-09-23 11:32:08 -0300

    Compração de Strings com outros meios

---
+++ Strings/EqualsString/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EqualsString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "GOL BOLINHA 1994";
+            if(texto.Equals("gol" , StringComparison.OrdinalIgnoreCase));
+                Console.WriteLine("Gol bolinha do ano de 1994");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strings/StartsWith/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace StartsWith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Bololo Haha";
+            Console.WriteLine(texto);
+            if (texto.StartsWith("Bololo"))
+            {
+                Console.WriteLine("Tá tranquilo tá favoravel ");
+            }
+            else
+            {
+                Console.WriteLine("Nada tranquilo");
+            }
+
+
+            var frase = "Caboco , tu gosta de farinha ?";
+            Console.WriteLine(frase);
+            if (frase.EndsWith("?" ))
+            {
+                Console.WriteLine("E de peixe ? ");
+                Console.WriteLine("TU É DOIIIIDOOOO");
+            }
+            else
+            {
+                Console.WriteLine("0 memes hoje ");
+            }
+
+
+          
+
+            Console.ReadLine();
+        }
+    }
+}
