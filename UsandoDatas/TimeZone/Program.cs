94dff3cc867edd03be6ba96cc2c7fcb20afe7e4e
Matheus <matheuznardi@gmail.com>
2023-10-02 18:19:21 -0300

    Novas Aulas -Datas

---
+++ UsandoDatas/TimeZone/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TimeZone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Utc pega uma data universal , sem fusos ex : +1 -3 
+            var dataTime = DateTime.UtcNow;
+
+            Console.WriteLine(dataTime);
+
+
+            //Utc para outros países 
+
+            var timeZoneAustralia = TimeZoneInfo.FindSystemTimeZoneById("Pacific/Auckland");
+            Console.WriteLine(timeZoneAustralia);
+
+            var horaAustralia = TimeZoneInfo.ConvertTimeFromUtc(dataTime, timeZoneAustralia);
+            Console.WriteLine(horaAustralia);
+
+            //Obter os Id 
+
+            var timeZones = TimeZoneInfo.GetSystemTimeZones();
+                foreach(var timeZone in timeZones)
+            {
+                Console.WriteLine(timeZone.Id);
+                Console.WriteLine(timeZone);
+                Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(dataTime , timeZone));
+                Console.WriteLine("---------------------------------");
+            }
+
+            Console.ReadLine() ;
+        }
+    }
+}
---
+++ UsandoDatas/Timespan/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Timespan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TimeSpan uma "unidade de medida para tempo"
+
+            var timeSpanSegundos = new TimeSpan( 0 ,0, 20);
+          
+            var horaDeEntrada = new TimeSpan( 8 , 0 , 0 );
+            var saida = new TimeSpan( 14 , 0 , 0 );
+
+            double tempoDeTrabalho = (saida - horaDeEntrada).TotalHours;
+            if( tempoDeTrabalho < 8 )
+            {
+                Console.WriteLine("Voce trabalhou menos de 8 horas ");
+            }
+            else
+            {
+                Console.WriteLine("Voce trabalhou mais de 8 horas ");
+            }
+
+            int hora, min = 0;
+            hora = int.Parse(Console.ReadLine());
+            min = int.Parse(Console.ReadLine());
+            var inputUsuario = new TimeSpan( hora , min, 0 );
+            Console.WriteLine(inputUsuario);
+            Console.ReadLine();
+        }
+    }
+}
