1282fbbed3ba5c9c157bb8767060ca5c1b28f79d
Matheus <matheuznardi@gmail.com>
2023-10-03 17:33:05 -0300

    Formatacao de moedas

---
+++ Moedas/MathInCsharp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MathInCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numero = 455.43m;
+            
+            //Arredonda para um num maior
+            Console.WriteLine(Math.Ceiling(numero));
+           
+
+            //Arredonda para um num menor
+            Console.WriteLine(Math.Floor(numero));
+
+            //Arredonda "tirando" as casas decimais 
+            Console.WriteLine(Math.Round(numero));
+
+
+            //Existem inumeras funções 
+
+            Console.WriteLine(Math.Pow(10.0 , 2.0));
+            Console.WriteLine(Math.Sqrt(64));
+            Console.WriteLine(Math.Abs(-78.9));
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Moedas/TipoParaMoedas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace TipoParaMoedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal valor = 100.34m;
+            var br = CultureInfo.CreateSpecificCulture("pt-BR");
+            var en_US = CultureInfo.CreateSpecificCulture("en-US");
+            var en_UK = CultureInfo.CreateSpecificCulture("en-UK");
+            Console.WriteLine("{0}" , NumberFormatInfo.CurrentInfo.CurrencySymbol);
+            Console.WriteLine(valor.ToString("C" , en_UK));
+            Console.WriteLine(valor.ToString("C" , en_US));
+
+            
+
+
+           
+
+            //Tipos de formatação
+            /*  
+             b = binario
+             c= Moeda 
+             d = decimal
+             e = exponencial
+             g = geral 
+             p = porcentagem 
+             x = hexadecimal
+             
+             
+             
+             */
+
+            Console.ReadLine();
+        }
+    }
+}
