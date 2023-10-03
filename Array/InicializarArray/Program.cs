4e9b8e9d9eeba8de0945f575cadda95a88867960
Matheus <matheuznardi@gmail.com>
2023-10-03 18:11:17 -0300

    Arrys

---
+++ Array/InicializarArray/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace InicializarArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[2] { 23 ,45 };
+            Console.WriteLine(num[0]);
+            Console.WriteLine(num[1]);
+
+            var countVogais = 0;
+            char[] vogais = new char[5]
+            {
+                'a' , 'e' , 'i' ,'o' ,'u'
+            };
+
+            var nome = "Matheus";
+
+            for(int i = 0; i < nome.Length  ; i++)
+            {
+                if (vogais.Contains(nome[i]))
+                {
+                    countVogais++;
+                }
+            }
+
+            Console.WriteLine("Seu nome possuiu vogais {0}" , countVogais);
+
+            Console.ReadLine();
+
+        }
+    }
+}
